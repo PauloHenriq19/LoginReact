8fb2f866760da354f2b8c6c5ff2c747d71dcaedb
Paulo Henrique <pauloh.pfigueiredo@gmail.com>
2022-12-21 22:16:24 -0300

    criando campo de login e senha

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import LoginPage from "./pages/LoginPage";
+import HomePage from './pages/LoginPage/HomePage';
+
+function App() {
+  return (
+    <div className="app">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
