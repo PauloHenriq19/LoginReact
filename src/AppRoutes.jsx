6b351d6ca5eae099b07d4142ae0fb4b2b59a0448
Paulo Henrique <pauloh.pfigueiredo@gmail.com>
2023-01-04 22:37:02 -0300

    implementando a lógica do login

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import {
     BrowserRouter as Router,
     Route,
@@ -7,13 +9,22 @@ import {
 import HomePage from './pages/HomePage';
 import LoginPage from "./pages/LoginPage";
 
+import { AuthContext } from "./contexts/auth";
+
 const AppRoutes = () => {
+const [user, setUser] = useState(null);
+
+const login = (email, password) => {};
+
+const logout = () => {};
     return(
         <Router>
-            <Routes>
-                <Route exact path="/login" element={<LoginPage/>} />
-                <Route exact path="/" element={<HomePage/>} />
-            </Routes>
+            <AuthContext.Provider value={{authenticated: !!user, user, login}}>
+                <Routes>
+                    <Route exact path="/login" element={<LoginPage/>} />
+                    <Route exact path="/" element={<HomePage/>} />
+                </Routes>
+            </AuthContext.Provider>
         </Router>
     )
 }
