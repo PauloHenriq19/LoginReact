ebce5cd0d1c4bdfd9b089e0dd730c9b1b2bc90a7
Paulo Henrique <pauloh.pfigueiredo@gmail.com>
2023-01-09 22:42:17 -0300

    finalizando autenticação de login e incluindo parametrização do token

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 
 import { useNavigate } from "react-router-dom";
+import { api, createSession } from "../services/api";
 
 export const AuthContext = createContext();
 
@@ -11,35 +12,43 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const recoveredUser = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
 
     if (recoveredUser) {
       setUser(JSON.parse(recoveredUser));
+      api.defaults.headers.Authorization = `Bearer ${token}`;
     }
 
     setLoading(false);
   }, []);
 
-  const login = (email, password) => {
-    console.log("login auth", { email, password });
+  const login = async (email, password) => {
+    const response = await createSession(email, password);
+
+    console.log("login", response.data);
 
     // api criar uma session
 
-    const loggedUser = {
-      id: "123",
-      email,
-    };
+    const loggedUser = response.data.user;
+    const token = response.data.token;
 
     localStorage.setItem("user", JSON.stringify(loggedUser));
+    localStorage.setItem("token", token);
 
-    if (password === "secret") {
-      setUser(loggedUser);
-      navigate("/");
-    }
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    setUser(loggedUser);
+    navigate("/");
   };
 
   const logout = () => {
     console.log("logout");
+
     localStorage.removeItem("user");
+    localStorage.removeItem("token");
+
+    api.defaults.headers.Authorization = null;
+
     setUser(null);
     navigate("/login");
   };
